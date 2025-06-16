2457c8fcc13b6facf442f7195cd837a7d6a47a03
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-06-16 23:59:46 +0200

    Remove crate manager build script

--- build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-	crate_manager::generate_exports_for_crates_in_working_dir().expect("Could not generate auto-lib-exports.");
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-// auto-export
 mod task;
 mod task_event;
 mod task_system;
 mod task_system_u;
 mod task_u;
+
 pub use task::*;
 pub use task_event::*;
 pub use task_system::*;
