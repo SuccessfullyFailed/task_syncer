0957cdfe8ea068bbe08aeffc24951a67170ee055
SuccessfullyFailed <renzogerritzen@gmail.com>
2026-04-03 19:51:21 +0200

    Use modifications queue to get rid of sleep and delays in the system runner thread

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 mod system;
 mod system_u;
-mod scheduler;
-mod scheduler_u;
 mod task;
 mod task_u;
 mod task_handler;
 mod task_handler_u;
 
 pub use system::*;
-pub use scheduler::*;
 pub use task::*;
 pub use task_handler::*;
